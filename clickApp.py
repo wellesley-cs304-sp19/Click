0c9b0af539764ee61984e69d5cdd0e69083bf8be
Dana F <dfeinsch@wellesley.edu>
2019-04-18 23:06:24 +0000

    Added student and studentProfile html pages and corresponding queries and code in python files

--- clickApp.py
+++ clickApp.py
@@ -19,6 +19,42 @@ conn = clickDatabase.getConn('clickdb')
 def home():
     return render_template('home.html')
 
+#route to page that student sees when first log in
+@app.route("/student/<email>")
+def studentPage(email):
+    return render_template('student.html',
+                            email=email)
+
+#route to page that allows student to view profile and add skills    
+@app.route("/studentProfile/<email>", methods = ['GET', 'POST'])
+def studentProfile(email):
+    #if GET, renders page with all information about student in database
+    if request.method == 'GET':
+        studentInfo = clickDatabase.getStudent(conn, email)
+        skills = clickDatabase.studentSkills(conn, email)
+        return render_template('studentProfile.html',
+                            name = studentInfo['name'],
+                            email = studentInfo['email'],
+                            skills = skills)
+    #if POST, either adding or removing a skill
+    else:
+        #removing skill
+        if request.form['submit'] == 'Remove':
+            skill = request.form.get('skill')
+            clickDatabase.removeSkill(conn, email, skill)
+            return render_template('studentProfile.html',
+                            name = studentInfo['name'],
+                            email = studentInfo['email'],
+                            skills = skills)
+        #adding skill
+        else:
+            newSkill = request.form.get('newSkill')
+            clickDatabase.addSkill(conn, email, newSkill)
+            return render_template('studentProfile.html',
+                            name = studentInfo['name'],
+                            email = studentInfo['email'],
+                            skills = skills)
+
 #put rest of routes/functions here
 
 if __name__ == '__main__':
--- clickDatabase.py
+++ clickDatabase.py
@@ -12,9 +12,47 @@ def getConn(db):
                            db=db)
     conn.autocommit(True)
     return conn
-    
 
-#put our functions here
+#gets student's profile from database
+def getStudent(conn, email):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''Select * from user where email = %s''', [email])
+    return curs.fetchone()
+
+#get all student's skills from database    
+def studentSkills(conn, email):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    #use inner join to get list of skills
+    curs.execute('''select skill from skills inner join hasSkill using (sid)
+    where hasSkill.email = %s''', [email])
+    return curs.fetchall()
+  
+#removes skill from student  
+def removeSkill(conn, email, skill):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''Select sid from skills where skill = %s''', [skill])
+    skillNum = curs.fetchone().values()[0]
+    nr = curs.execute('''delete from hasSkill where sid = %s and email = %s''', [skillNum, email])
+    return nr
+
+#adds skill to student    
+def addSkill(conn, email, skill):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    curs.execute('''Select sid from skills where skill = %s''', [skill])
+    skillNum = curs.fetchone()
+    #if skill not in skills table, add it
+    if skillNum == None:
+        curs.execute('''insert into skills(skill) values (%s)''', [skill])
+        curs.execute('''Select sid from skills where skill = %s''', [skill])
+        skillNum = curs.fetchone()
+    #continue with inserting email and skill into hasSkill table
+    nr = curs.execute('''insert into hasSkill(email, sid) values (%s, %s)''', [email, skillNum.values()[0]])
+    return nr
+    
+#put rest of our functions here
    
 if __name__ == '__main__':
     conn = getConn('clickdb')
+    #addSkill(conn, "student2@gmail.com", "editing")
+    #print(removeSkill(conn, "student1@gmail.com", "math tutoring"))
