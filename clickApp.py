920ecaff29495877111fcbcbecd692e8c6d5374f
Erica Ma <zma@wellesley.edu>
2019-05-11 01:56:50 +0000

    add search student functions

--- clickApp.py
+++ clickApp.py
@@ -2,19 +2,13 @@
 #Dana, Erica, and Gabby
 
 from flask import (Flask, url_for, render_template, request, redirect, flash,session)
-<<<<<<< HEAD
-=======
 from datetime import date
->>>>>>> origin/master
 import random,math
 import MySQLdb
 import sys
 import bcrypt
 import clickDatabase
-<<<<<<< HEAD
-=======
 from connection import getConn
->>>>>>> origin/master
 from functools import wraps
 import os
 
@@ -24,22 +18,14 @@ app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
                                           'abcdefghijklmnopqrstuvxyz' +
                                           '0123456789'))
                            for i in range(20) ])
-<<<<<<< HEAD
-                           
-conn = clickDatabase.getConn('clickdb')
-=======
                           
->>>>>>> origin/master
 
 #route to the home page
 @app.route("/")
 def home():
     return render_template('home.html')
     
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 '''Since in the future, we want to have a user visiting
     a site to be redirected to the login page if they are
     not already logged in. We use a decorator to solve 
@@ -56,25 +42,15 @@ def login_required(f):
             return redirect(url_for('login'))
     return decorated_fcn()
 
-<<<<<<< HEAD
-=======
     
->>>>>>> origin/master
 #route to the login page
 @app.route('/login/',methods=['GET','POST'])
 def login():
     #the code below works well
-<<<<<<< HEAD
-    #return render_template('login.html')
-    
-    #somehow the code below gives Bad Request: The browser (or proxy) sent a request that this server could not understand.
-    try:
-=======
     if request.method=='GET':
         return render_template('login.html')
     else:
         print("test enter")
->>>>>>> origin/master
         username=request.form['username']
         password=request.form['password']
         conn=clickDatabase.getConn('clickdb')
@@ -82,36 +58,17 @@ def login():
         curs.execute('select password from user where email=%s',[username])
         row=curs.fetchone()
         if row is None:
-<<<<<<< HEAD
-            flash('Login failed. Please register or try again')
-            return redirect(url_for('home'))
-        hashed=row['password']
-        if bcrypt.hashpw(password.encode('utf-8'),hashed.encode('utf-8'))==hashed:
-=======
             print("test enter1")
             flash('Login failed. Please register or try again')
             return redirect(url_for('home'))
         hashed=row['password']
         if (bcrypt.hashpw(password.encode('utf-8'),hashed.encode('utf-8'))==hashed.encode('utf-8')):
             print("test enter2")
->>>>>>> origin/master
             flash('Successfully logged in as'+username)
             session['username']=username
             session['logged_in']= True
             return redirect(url_for('home'))
         else:
-<<<<<<< HEAD
-            flash('Login failed. Please register or try again')
-            return redirect(url_for('login'))
-    except Exception as err:
-        flash('From submission error'+str(err))
-        return redirect(url_for('home'))
-        
-        
-#route to the register page
-@app.route('/register/')
-def register():
-=======
             print("test enter3")
             flash('Login failed. Please register or try again')
             return redirect(url_for('login'))
@@ -130,7 +87,6 @@ def redirectToLogin():
 def register():
     '''allows user to register by creating a username and password;
     if username is unique, encrypts passwords and store it'''
->>>>>>> origin/master
     try:
         username=request.form['username']
         password1=request.form['password1']
@@ -140,11 +96,7 @@ def register():
             return redirect(url_for('login'))
         hashed=bcrypt.hashpw(password1.encode('utf-8'),bcrypt.gensalt())
         #hashed=password1
-<<<<<<< HEAD
-        conn=clickDatabase.getConn('clickdb')
-=======
         conn=getConn()
->>>>>>> origin/master
         curs=conn.cursor(MySQLdb.cursors.DictCursor)
         curs.execute('select email from user where email=%s',[username])
         row=curs.fetchone()
@@ -163,10 +115,7 @@ def register():
 #route to logout
 @app.route('/logout/')
 def logout():
-<<<<<<< HEAD
-=======
     '''logging out the current user'''
->>>>>>> origin/master
     try:
         if 'username' in session:
             username = session['username']
@@ -181,18 +130,12 @@ def logout():
         flash('Error Message: '+str(err))
         return redirect( url_for('home') )
         
-<<<<<<< HEAD
-#route to page that student sees when first log in
-=======
 '''route to page that student sees when first log in'''
->>>>>>> origin/master
 @app.route("/student/<email>")
 #@login_required
 def studentPage(email):
     return render_template('student.html',
                             email=email)
-<<<<<<< HEAD
-=======
                             
 @app.route('/reset/', methods=['GET', 'POST'])
 def reset():
@@ -204,7 +147,6 @@ def reset():
     #in project.html, reset projects; return master list of projects
     else: 
         return redirect('projects')
->>>>>>> origin/master
 
 #route to page that job poster sees when first log in
 @app.route("/jobPoster/<email>")
@@ -212,56 +154,6 @@ def jobPosterPage(email):
     return render_template('jobPoster.html',
                             email=email)                           
 
-<<<<<<< HEAD
-#route to page that allows student to view profile and add skills    
-@app.route("/studentProfile/<email>", methods = ['GET', 'POST'])
-#@login_required
-def studentProfile(email):
-    studentInfo = clickDatabase.getStudent(conn, email)
-    skills = clickDatabase.studentSkills(conn, email)
-    #if GET, renders page with all information about student in database
-    if request.method == 'GET':
-        return render_template('studentProfile.html',
-                            name = studentInfo['name'],
-                            email = studentInfo['email'],
-                            skills = skills)
-    #if POST, either adding or removing a skill
-    else:
-        #removing skill
-        if request.form['submit'] == 'Remove':
-            skill = request.form.get('skill')
-            clickDatabase.removeSkill(conn, email, skill)
-            return redirect(url_for('studentProfile',
-                            email = studentInfo['email']))
-        #adding skill
-        else:
-            newSkill = request.form.get('newSkill')
-            clickDatabase.addSkill(conn, email, newSkill)
-            return redirect(url_for('studentProfile',
-                            email = studentInfo['email']))
-
-#route to page that allows job poster to see his/her current projects     
-@app.route("/project/<pid>", methods = ['GET', 'POST'])
-def project(pid):
-    #if GET, renders page with all information about that project in database
-    if request.method == 'GET':
-        projectInfo = clickDatabase.getProject(conn, pid)
-        return render_template('project.html',
-                            name = projectInfo['name'],
-                            minHours = projectInfo['minHours'],
-                            pay = projectInfo['pay'],
-                            location = projectInfo['location'],
-                            )
-
-# insert page
-@app.route('/insertProject/')
-def insertProject():
-    return render_template('insertProject.html')
-
-# insert page form handling 
-@app.route('/insertProject/', methods=['GET','POST'])
-def submit_insertProject():
-=======
 '''Route to page that allows student to view profile and add skills.
    The student profile is for the student that is currently logged in.
 '''
@@ -336,34 +228,32 @@ def jobs():
             filteredJobs = clickDatabase.searchJobs(conn, search)
             return render_template('jobs.html', jobs = filteredJobs)
 
-#route to page that allows job poster to see his/her current postings     
-@app.route("/posting/<pid>", methods = ['GET', 'POST'])
-def posting(pid):
+#route to page that allows job poster to see his/her current projects     
+@app.route("/project/<pid>", methods = ['GET', 'POST'])
+def project(pid):
     conn = clickDatabase.getConn('clickdb')
-    #if GET, renders page with all information about that posting in database
+    #if GET, renders page with all information about that project in database
     if request.method == 'GET':
-        postingInfo = clickDatabase.getPosting(conn, pid)
-        return render_template('posting.html',
-                            name = postingInfo['name'],
-                            minHours = postingInfo['minHours'],
-                            pay = postingInfo['pay'],
-                            location = postingInfo['location'],
+        projectInfo = clickDatabase.getProject(conn, pid)
+        return render_template('project.html',
+                            name = projectInfo['name'],
+                            minHours = projectInfo['minHours'],
+                            pay = projectInfo['pay'],
+                            location = projectInfo['location'],
                             )
 
 # insert page
-@app.route('/insertPosting/')
-def insertPosting():
-    return render_template('insertPosting.html')
+@app.route('/insertProject/')
+def insertProject():
+    return render_template('insertProject.html')
 
 # insert page form handling 
-@app.route('/insertPosting/', methods=['GET','POST'])
-def submit_insertPosting():
->>>>>>> origin/master
+@app.route('/insertProject/', methods=['GET','POST'])
+def submit_insertProject():
     conn = clickDatabase.getConn('clickdb')
     if request.method == 'POST':
     
         # checking database to see if the given pid is in use 
-<<<<<<< HEAD
         if (clickDatabase.search_project_pid(conn, request.form['project-pid'])) != None:
             flash('bad input: project\'s pid already in use.')
             return render_template('insertProject.html')
@@ -411,62 +301,12 @@ def selectProject():
     conn = clickDatabase.getConn('clickdb')
     allProjects = clickDatabase.find_allProjects(conn)
     return render_template('selectProject.html', allProjects=allProjects)
-=======
-        if (clickDatabase.search_posting_pid(conn, request.form['posting-pid'])) != None:
-            flash('bad input: project\'s pid already in use.')
-            return render_template('insertPosting.html')
-        
-        # checking if info is missing in input 
-        if ((request.form['posting-pid'] == "") or (request.form['posting-name'] == "") 
-        or (request.form['posting-pay'] == "") or (request.form['posting-minHours'] == "")
-        or (request.form['posting-location'] == "")):
-            if request.form['posting-pid'] == "":
-                flash('missing input: project\'s pid is missing.')
-                
-        
-            if request.form['posting-name'] == "":
-                flash('missing input: name is missing.')
-               
-            if request.form['posting-pay'] == "": 
-                flash('missing input: pay is missing.')
-                
-            if request.form['posting-minHours'] == "": 
-                flash('missing input: minimum hours is missing.')
-                
-            if request.form['posting-location'] == "": 
-                flash('missing input: location is missing.')
-                
-            return render_template('insertPosting.html')
-            
-        if ((request.form['posting-pid'] == "") and (request.form['posting-name'] == "") 
-        and (request.form['posting-pay'] == "") and (request.form['posting-minHours'] == "")
-        and (request.form['posting-location'] == "")):
-            
-            postingInfo = clickDatabase.search_posting_pid(conn, request.form['posting-pid'])
-            if postingInfo == None: 
-                clickDatabase.insert_posting(conn, request.form['posting-pid'], 
-                request.form['posting-name'], request.form['posting-pay'],
-                request.form['posting-minHours'], request.form['posting-location'])
-                flash('Posting {name} was created successfully'.format(title=request.form['posting-name']))
-
-            else:
-                flash("Posting already exists")
-            return redirect(url_for('updatePosting', pid = request.form['movie-posting']))
-
-# setting up page with postings
-@app.route('/selectPosting/')
-def selectPosting():
-    conn = clickDatabase.getConn('clickdb')
-    allPostings = clickDatabase.find_allPostings(conn)
-    return render_template('selectPosting.html', allPostings=allPostings)
->>>>>>> origin/master
     
 # returns true when a SQL query's result is not empty
 def isValid(results):
-    return results != None
+    return results is not None
     
 # select page form handling
-<<<<<<< HEAD
 @app.route('/selectProject/', methods=['GET','POST'])
 def select_project():
     conn = clickDatabase.getConn('clickdb')
@@ -476,17 +316,6 @@ def select_project():
     else: 
         flash('Please select a project')
         return render_template('selectProject.html')
-=======
-@app.route('/selectPosting/', methods=['GET','POST'])
-def select_posting():
-    conn = clickDatabase.getConn('clickdb')
-    pid = request.form.get('select-name') ###????
-    if isValid(pid):
-        return redirect(url_for('updatePosting', pid=pid))
-    else: 
-        flash('Please select a project posting')
-        return render_template('selectPosting.html')
->>>>>>> origin/master
         
 # search page
 @app.route('/searchStudent/')
@@ -500,15 +329,23 @@ def search_student():
     conn = clickDatabase.getConn('clickdb')
     email = clickDatabase.get_email(conn, name)
     if isValid(email):
-<<<<<<< HEAD
         return redirect(url_for('updateProject', email=email))
-=======
-        return redirect(url_for('updatePosting', email=email))
->>>>>>> origin/master
     else: 
         flash('Requested student does not exist')
         return render_template('searchStudent.html')
 
+@app.route("/students", methods = ['GET', 'POST'])
+def students():
+    conn = clickDatabase.getConn('clickdb')
+    if request.method == 'GET':
+        students = clickDatabase.getStudents(conn)
+        return render_template('students.html', students = students)
+    else:
+        if request.form['submit'] == 'Search':
+            search = request.form.get('searchsStudents')
+            filteredStudents = clickDatabase.searchStudents(conn, search)
+            return render_template('students.html', jobs = filteredStudents)
+
 if __name__ == '__main__':
     app.debug = True
-    app.run('0.0.0.0',8080)
+    app.run('0.0.0.0',8081)
--- clickDatabase.py
+++ clickDatabase.py
@@ -3,7 +3,6 @@
 
 import sys
 import MySQLdb
-<<<<<<< HEAD
 
 '''our clickdb connection'''
 def getConn(db):
@@ -14,18 +13,13 @@ def getConn(db):
     conn.autocommit(True)
     return conn
 
-#gets student's profile from database
-def getStudent(conn, email):
-    curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('''Select * from user where email = %s''', [email])
-    return curs.fetchone()
 
 #get all student's skills from database    
-=======
+
 import threading
 from connection import getConn
-from threading import lock
-import re
+
+
 
 '''Gets student's information (name, email) from database'''
 def getStudent(conn, email):
@@ -34,7 +28,6 @@ def getStudent(conn, email):
     return curs.fetchone()
 
 '''Returns results of SQL query to get student's skills from the database'''   
->>>>>>> origin/master
 def studentSkills(conn, email):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     #use inner join to get list of skills
@@ -42,17 +35,7 @@ def studentSkills(conn, email):
     where hasSkill.email = %s''', [email])
     return curs.fetchall()
     
-<<<<<<< HEAD
-#removes skill from student  
-def removeSkill(conn, email, skill):
-    curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('''Select sid from skills where skill = %s''', [skill])
-    skillNum = curs.fetchone().values()[0]
-    nr = curs.execute('''delete from hasSkill where sid = %s and email = %s''', [skillNum, email])
-    return nr
 
-#adds skill to student    
-=======
 '''Removes skill from student'''  
 def removeSkill(conn, email, skill):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -61,12 +44,12 @@ def removeSkill(conn, email, skill):
     return nr
 
 '''Adds skill to student'''    
->>>>>>> origin/master
+
 def addSkill(conn, email, skill):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
     curs.execute('''Select sid from skills where skill = %s''', [skill])
-<<<<<<< HEAD
+
     skillNum = curs.fetchone()
     #if skill not in skills table, add it
     if skillNum == None:
@@ -77,7 +60,7 @@ def addSkill(conn, email, skill):
     nr = curs.execute('''insert into hasSkill(email, sid) values (%s, %s)''', [email, skillNum.values()[0]])
     return nr
     
-=======
+
     skillQuery = curs.fetchone() #stores results from query to get skills
     #if skill not in skills table, add it
     if skillQuery == None:
@@ -113,56 +96,40 @@ def searchJobs(conn, search):
                     where name like %s''', ['%'+ search + '%'])
     return curs.fetchall()
     
->>>>>>> d588fb0cdd2bd8b6428ac0874a708f5512e8ba97
->>>>>>> origin/master
+
 #adds a new user
 def addUser(conn,email,password):
     curs=conn.cursor()
     newrow=curs.execute('''insert into user(email,password) values (%s,%s)''',[email,password])
     return newrow
     
-<<<<<<< HEAD
+
 #put rest of our functions here
 
 
 #get info about a project 
 def getProject(conn, pid):
-=======
-#get info about a posting 
-def getPosting(conn, pid):
->>>>>>> origin/master
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('''Select * from project where pid = %s''', [pid])
     return curs.fetchone()
    
 #SQL query to get pid, name, pay, minimum hours, location from project table using the pid    
-<<<<<<< HEAD
+
 def search_project_pid(conn, pid): 
-=======
-def search_posting_pid(conn, pid): 
->>>>>>> origin/master
     curs = conn.cursor()
     curs.execute('''select pid,name,pay,minHours,
     location from project where pid = %s;''',
                     [pid])
     return curs.fetchone()  
 
-<<<<<<< HEAD
 #SQL code to insert project using pid, name, pay, minimum hours, location    
 def insert_project(conn, pid, name, pay, minHours, location): 
     curs = conn.cursor()
-    curs.execute('''insert into project(pid,name,minHours, location)
-=======
-#SQL code to insert posting using pid, name, pay, minimum hours, location    
-def insert_posting(conn, pid, name, pay, minHours, location): 
-    curs = conn.cursor()
-    curs.execute('''insert into posting(pid,name,minHours, location)
->>>>>>> origin/master
-values (%s,%s,%s, %s);''',
+    curs.execute('''insert into project(pid,name,minHours, location) values (%s,%s,%s, %s);''',
                     [pid,name,minHours,location])
     return curs
   
-<<<<<<< HEAD
+
 #SQL query to get a list of all current projects    
 def find_allProjects(conn):
     curs = conn.cursor()
@@ -170,14 +137,17 @@ def find_allProjects(conn):
     location from project;''')
     allProjects = curs.fetchall()
     return allProjects;
-=======
-#SQL query to get a list of all current postings    
-def find_allPostings(conn):
-    curs = conn.cursor()
-    curs.execute('''select pid,name,pay,minHours, 
-    location from project;''')
-    allPostings = curs.fetchall()
-    return allPostings;
+
+def getStudents(conn):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select email, name, active, userType from project''')
+    return curs.fetchall()
+    
+def searchStudents(conn, search):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('''select email, name, active, userType from project
+                    where name like %s''', ['%'+ search + '%'])
+    return curs.fetchall()
 
 
 #add a new job posting
@@ -188,7 +158,6 @@ def addProject(conn,project_dict):
     result=curs.fetchall()
     return(result[0][0])
 
->>>>>>> origin/master
 
 #SQL query to get specifically email of a student with a given name 
 def get_email(conn, name):
@@ -197,14 +166,6 @@ def get_email(conn, name):
     email = curs.fetchone()
     return email[0]
 
-<<<<<<< HEAD
-   
-if __name__ == '__main__':
-    conn = getConn('clickdb')
-    #addSkill(conn, "student2@gmail.com", "editing")
-    #print(removeSkill(conn, "student1@gmail.com", "math tutoring"))
-    #studentSkills(conn, "student1@gmail.com")
-=======
 
    
 if __name__ == '__main__':
@@ -212,4 +173,4 @@ if __name__ == '__main__':
     #addSkill(conn, "student2@gmail.com", "public speaking")
     #print(removeSkill(conn, "student1@gmail.com", "math tutoring"))
     #print(studentSkills(conn, "student1@gmail.com"))
->>>>>>> origin/master
+
